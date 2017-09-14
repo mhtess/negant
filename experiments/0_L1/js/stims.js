4aa44ccc1ca02e05fc60f8603f4ae542cddca441
mhtess <mtessler@stanford.edu>
2017-09-14 16:40:47 +0200

    reduce number of items for pilot

--- experiments/0_L1/js/stims.js
+++ experiments/0_L1/js/stims.js
@@ -1,14 +1,16 @@
 var stimuli = [
   { positive: "happy",  antonym: "unhappy", referent: "person", noun: "level of happiness"},
   { positive: "intelligent",  antonym: "unintelligent", referent: "person", noun:  "level of intelligence"},
-  { positive: "successful",  antonym: "unsuccessful", referent: "person", noun:  "level of successfulness" },
+  // { positive: "successful",  antonym: "unsuccessful", referent: "person", noun:  "level of successfulness" },
   { positive: "polite",  antonym: "impolite", referent: "person", noun:  "level of politeness"},
-  { positive: "honest",  antonym: "dishonest", referent: "person", noun:  "level of honesty"},
+  // { positive: "honest",  antonym: "dishonest", referent: "person", noun:  "level of honesty"},
   { positive: "interesting",  antonym: "uninteresting", referent: "person", noun:  "level of interestingness"},
   { positive: "attractive",  antonym: "unattractive", referent: "person", noun:  "level of attractiveness"},
-  { positive: "educated",  antonym: "uneducated", referent: "person", noun:  "level of education"},
+  // { positive: "educated",  antonym: "uneducated", referent: "person", noun:  "level of education"},
   { positive: "forgiving",  antonym: "unforgiving", referent: "person", noun:  "level of forgivingness"},
-  { positive: "tolerant",  antonym: "intolerant", referent: "person", noun:  "level of tolerance"}
+  // { positive: "tolerant",  antonym: "intolerant", referent: "person", noun:  "level of tolerance"}
+
+
   // { positive: "likely",  antonym: "unlikely", referent: "event_ref" },
   // { positive: "common",  antonym: "uncommon", referent: "event_ref" },
   // { positive: "frequent",  antonym: "infrequent", referent: "event_ref" }
@@ -21,4 +23,12 @@ var referents = {
   objects: {head: "There are", object: "things"}
 }
 
-var semanticPoints = [0.1, 0.3, 0.4, 0.5, 0.6, 0.7, 0.9];
+var semanticPoints = [
+  0.1,
+  0.3,
+  0.4,
+  // 0.5,
+  0.6,
+  0.7,
+  0.9
+];
