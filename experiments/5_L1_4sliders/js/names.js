5c6c617108fc04deb3004eec9cb29b5c7a0ff618
mhtess <mtessler@stanford.edu>
2018-01-11 12:37:33 -0800

    4 slider version of new task

---
+++ experiments/5_L1_4sliders/js/names.js
@@ -0,0 +1,457 @@
+var characters = [
+{
+	name: "Daniel",
+	gender: "male"
+},
+{
+	name: "David",
+	gender: "male"
+},
+{
+	name: "Alexander",
+	gender: "male"
+},
+{
+	name: "Gabriel",
+	gender: "male"
+},
+{
+	name: "Anthony",
+	gender: "male"
+},
+{
+	name: "William",
+	gender: "male"
+},
+{
+	name: "John",
+	gender: "male"
+},
+{
+	name: "Tom",
+	gender: "male"
+},
+{
+	name: "Lucas",
+	gender: "male"
+},
+{
+	name: "Michael",
+	gender: "male"
+},
+{
+	name: "Eric",
+	gender: "male"
+},
+{
+	name: "Cameron",
+	gender: "male"
+},
+{
+	name: "Stephen",
+	gender: "male"
+},
+{
+	name: "Lee",
+	gender: "male"
+},
+{
+	name: "Greg",
+	gender: "male"
+},
+{
+	name: "Jeremy",
+	gender: "male"
+},
+{
+	name: "Charles",
+	gender: "male"
+},
+{
+	name: "Andrew",
+	gender: "male"
+},
+{
+	name: "Samuel",
+	gender: "male"
+},
+{
+	name: "Dylan",
+	gender: "male"
+},
+{
+	name: "Angela",
+	gender: "female"
+},
+{
+	name: "Jennifer",
+	gender: "female"
+},
+{
+	name: "Monica",
+	gender: "female"
+},
+{
+	name: "Melinda",
+	gender: "female"
+},
+{
+	name: "Veronica",
+	gender: "female"
+},
+{
+	name: "Alicia",
+	gender: "female"
+},
+{
+	name: "Maya",
+	gender: "female"
+},
+{
+	name: "Sophia",
+	gender: "female"
+},
+{
+	name: "Kim",
+	gender: "female"
+},
+{
+	name: "Laura",
+	gender: "female"
+},
+{
+	name: "Julia",
+	gender: "female"
+},
+{
+	name: "Michelle",
+	gender: "female"
+},
+{
+	name: "Stephanie",
+	gender: "female"
+},
+{
+	name: "Claire",
+	gender: "female"
+},
+{
+	name: "Kathleen",
+	gender: "female"
+},
+{
+	name: "Sasha",
+	gender: "female"
+},
+{
+	name: "Diana",
+	gender: "female"
+},
+{
+	name: "Alison",
+	gender: "female"
+},
+{
+	name: "Emma",
+	gender: "female"
+},
+{
+	name: "Mia",
+	gender: "female"
+}
+]
+
+
+var maleCharacters = [
+{
+	name: "Daniel",
+	gender: "male"
+},
+{
+	name: "David",
+	gender: "male"
+},
+{
+	name: "Alexander",
+	gender: "male"
+},
+{
+	name: "Gabriel",
+	gender: "male"
+},
+{
+	name: "Anthony",
+	gender: "male"
+},
+{
+	name: "William",
+	gender: "male"
+},
+{
+	name: "John",
+	gender: "male"
+},
+{
+	name: "Tom",
+	gender: "male"
+},
+{
+	name: "Lucas",
+	gender: "male"
+},
+{
+	name: "Michael",
+	gender: "male"
+},
+{
+	name: "Eric",
+	gender: "male"
+},
+{
+	name: "Cameron",
+	gender: "male"
+},
+{
+	name: "Stephen",
+	gender: "male"
+},
+{
+	name: "Lee",
+	gender: "male"
+},
+{
+	name: "Greg",
+	gender: "male"
+},
+{
+	name: "Kyle",
+	gender: "male"
+},
+{
+	name: "Ben",
+	gender: "male"
+},
+{
+	name: "Josh",
+	gender: "male"
+},
+{
+	name: "Robert",
+	gender: "male"
+},
+{
+	name: "Ronald",
+	gender: "male"
+},
+{
+	name: "Jacob",
+	gender: "male"
+},
+{
+	name: "Lewis",
+	gender: "male"
+},
+{
+	name: "Stephen",
+	gender: "male"
+},
+{
+	name: "Derrick",
+	gender: "male"
+},
+{
+	name: "Christopher",
+	gender: "male"
+},
+{
+	name: "Brian",
+	gender: "male"
+},
+{
+	name: "Vince",
+	gender: "male"
+},
+{
+	name: "Ted",
+	gender: "male"
+},
+{
+	name: "Scott",
+	gender: "male"
+},
+{
+	name: "Seth",
+	gender: "male"
+},
+{
+	name: "Martin",
+	gender: "male"
+},
+{
+	name: "Doug",
+	gender: "male"
+},
+{
+	name: "George",
+	gender: "male"
+},
+{
+	name: "Tim",
+	gender: "male"
+},
+{
+	name: "Jack",
+	gender: "male"
+},
+{
+	name: "Fred",
+	gender: "male"
+}]
+
+
+
+var femaleCharacters = [
+{
+	name: "Angela",
+	gender: "female"
+},
+{
+	name: "Jennifer",
+	gender: "female"
+},
+{
+	name: "Monica",
+	gender: "female"
+},
+{
+	name: "Melinda",
+	gender: "female"
+},
+{
+	name: "Veronica",
+	gender: "female"
+},
+{
+	name: "Alicia",
+	gender: "female"
+},
+{
+	name: "Maya",
+	gender: "female"
+},
+{
+	name: "Sophia",
+	gender: "female"
+},
+{
+	name: "Kim",
+	gender: "female"
+},
+{
+	name: "Laura",
+	gender: "female"
+},
+{
+	name: "Julia",
+	gender: "female"
+},
+{
+	name: "Michelle",
+	gender: "female"
+},
+{
+	name: "Stephanie",
+	gender: "female"
+},
+{
+	name: "Claire",
+	gender: "female"
+},
+{
+	name: "Kathleen",
+	gender: "female"
+},
+{
+	name: "Rebecca",
+	gender: "female"
+},
+{
+	name: "Hillary",
+	gender: "female"
+},
+{
+	name: "Jessica",
+	gender: "female"
+},
+{
+	name: "Natalie",
+	gender: "female"
+},
+{
+	name: "Kristina",
+	gender: "female"
+},
+{
+	name: "Esther",
+	gender: "female"
+},
+{
+	name: "Elizabeth",
+	gender: "female"
+},
+{
+	name: "Crystal",
+	gender: "female"
+},
+{
+	name: "Theresa",
+	gender: "female"
+},
+{
+	name: "Janet",
+	gender: "female"
+},
+{
+	name: "Linda",
+	gender: "female"
+},
+{
+	name: "Elisa",
+	gender: "female"
+},
+{
+	name: "Ashley",
+	gender: "female"
+},
+{
+	name: "Shannon",
+	gender: "female"
+},
+{
+	name: "Tina",
+	gender: "female"
+},
+{
+	name: "Kelly",
+	gender: "female"
+},
+{
+	name: "Bridgette",
+	gender: "female"
+},
+{
+	name: "Amber",
+	gender: "female"
+},
+{
+	name: "Jackie",
+	gender: "female"
+},
+{
+	name: "Brooke",
+	gender: "female"
+},
+{
+	name: "Nicole",
+	gender: "female"
+}]
---
+++ experiments/5_L1_4sliders/js/stims.js
@@ -0,0 +1,73 @@
+var stimuli = [
+  // pilot 1
+  { positive: "happy",  antonym: "unhappy", referent: "person", negation: "morphological", endpoints: {low: "unhappiest", high: "happiest"}},
+  { positive: "intelligent",  antonym: "unintelligent", referent: "person",  negation: "morphological"},
+  { positive: "polite",  antonym: "impolite", referent: "person",  negation: "morphological"},
+  { positive: "interesting",  antonym: "uninteresting", referent: "person", negation: "morphological"},
+  { positive: "attractive",  antonym: "unattractive", referent: "person", negation: "morphological"},
+  // { positive: "forgiving",  antonym: "unforgiving", referent: "person", negation: "morphological"},
+  // end of initial set
+
+  // 3_L1
+  { positive: "successful",  antonym: "unsuccessful", referent: "person",  negation: "morphological"},
+  { positive: "honest",  antonym: "dishonest", referent: "person", negation: "morphological"},
+  { positive: "educated",  antonym: "uneducated", referent: "person", negation: "morphological"},
+
+  // { positive: "tolerant",  antonym: "intolerant", referent: "person",  negation: "morphological"},
+
+  { positive: "friendly",  antonym: "unfriendly", referent: "person", negation: "morphological" },
+  // { positive: "moral",  antonym: "immoral", referent: "person", negation: "morphological"},
+  { positive: "mature",  antonym: "immature", referent: "person", negation: "morphological"},
+
+  /// overflow
+  // { positive: "sane",  antonym: "insane", referent: "person", negation: "morphological"},
+  // { positive: "reasonable",  antonym: "unreasonable", referent: "person", negation: "morphological"},
+
+  // lexical antonyms
+  // 3_L1
+  { positive: "tall",  antonym: "short", referent: "person", negation: "lexical", endpoints: {low: "shortest", high: "tallest"}},
+  { positive: "rich",  antonym: "poor", referent: "person", negation: "lexical", endpoints: {low: "poorest", high: "richest"}},
+  { positive: "fat",  antonym: "skinny", referent: "person", negation: "lexical", endpoints: {low: "skinniest", high: "fattest"}},
+
+  // { positive: "old",  antonym: "young", referent: "person", negation: "lexical", endpoints: {low: "youngest", high: "oldest"}},
+  { positive: "hard-working",  antonym: "lazy", referent: "person", negation: "lexical"},
+  { positive: "brave",  antonym: "cowardly", referent: "person",  negation: "lexical"},
+  //
+
+  // overflow
+  { positive: "beautiful",  antonym: "ugly", referent: "person", negation: "lexical"},
+  // { positive: "anxious",  antonym: "calm", referent: "person", negation: "lexical"},
+  { positive: "wise",  antonym: "foolish", referent: "person", negation: "lexical"},
+
+  { positive: "proud",  antonym: "humble", referent: "person", negation: "lexical"},
+  { positive: "strong",  antonym: "weak", referent: "person", negation: "lexical", endpoints: {low: "weakest", high: "strongest"}},
+  { positive: "loud",  antonym: "quiet", referent: "person", negation: "lexical"}
+
+  // stage level?
+  // { positive: "drunk",  antonym: "sober", referent: "person"},
+  // { positive: "warm",  antonym: "cold", referent: "person"},
+
+
+  // is it going to rain?
+  // { positive: "likely",  antonym: "unlikely", referent: "event_ref" },
+  // { positive: "probable",  antonym: "improbable", referent: "event_ref" },
+  // { positive: "common",  antonym: "uncommon", referent: "event_ref" },
+  // { positive: "frequently",  antonym: "infrequently", referent: "event_ref" },
+  // { positive: "many",  antonym: "few", referent: "objects" }
+]
+
+var referents = {
+  person: {head: ["He is", "She is"], object: "person"},
+  event_ref: {head : ["It is"], object: "thing"},
+  objects: {head: "There are", object: "things"}
+}
+
+var semanticPoints = [
+  0.1,
+  0.3,
+  0.4,
+  // 0.5,
+  0.6,
+  0.7,
+  0.9
+];
