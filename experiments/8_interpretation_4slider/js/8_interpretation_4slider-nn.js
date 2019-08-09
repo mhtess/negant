e31bc8fbd1371e9f70fb9c9c6689aecd61690dda
MH Tessler <mht137@gmail.com>
2019-08-09 10:37:04 -0400

    update unique turker

--- experiments/8_interpretation_4slider/js/8_interpretation_4slider-nn.js
+++ experiments/8_interpretation_4slider/js/8_interpretation_4slider-nn.js
@@ -395,7 +395,7 @@ function init() {
 
   repeatWorker = false;
   (function(){
-      var ut_id = "mht-negant-L1-20190215";
+      var ut_id = "mht-negant-L1-20190809";
       if (UTWorkerLimitReached(ut_id)) {
         $('.slide').empty();
         repeatWorker = true;
